d83d2b7d6b4892098301c138e6f8d6e17a825144
igi <igimernapp@gmail.com>
2024-07-17 10:32:08 +0300

    firebase service account to use enironment variables

--- backend/utils/firebaseFunctions.js
+++ backend/utils/firebaseFunctions.js
@@ -1,17 +1,36 @@
 import fs from 'fs/promises';
 import admin from "firebase-admin"
-const readJsonFile = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath, 'utf-8');
-    const jsonData = JSON.parse(data);
-    return jsonData;
-  } catch (error) {
-    console.error('Error reading JSON file:', error);
-    throw error;
-  }
+// const readJsonFile = async (filePath) => {
+//   try {
+//     const data = await fs.readFile(filePath, 'utf-8');
+//     const jsonData = JSON.parse(data);
+//     return jsonData;
+//   } catch (error) {
+//     console.error('Error reading JSON file:', error);
+//     throw error;
+//   }
+// };
+// const jsonFilePath = './igi-leb-8245c-firebase-adminsdk-m4wg4-817b2e990c.json';
+
+
+
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id, 
+  private_key: process.env.private_key.replace(/\\n/g, '\n'),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id, 
+  auth_uri: process.env.auth_uri, 
+  token_uri: process.env.token_uri, 
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url, 
+  client_x509_cert_url: process.env.client_x509_cert_url, 
+  universe_domain: process.env.universe_domain 
 };
-const jsonFilePath = './igi-leb-8245c-firebase-adminsdk-m4wg4-817b2e990c.json';
-const serviceAccount = await readJsonFile(jsonFilePath);
+
+
+
+// const serviceAccount = await readJsonFile(jsonFilePath);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "gs://igi-leb-8245c.appspot.com", 
