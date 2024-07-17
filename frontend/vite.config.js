344b74b7e1db5ab8665a38792275efe82af26625
igi <igimernapp@gmail.com>
2024-07-17 12:56:33 +0300

    modified base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api": {
-        target: "/", 
+        target: "https://igi-web-app.onrender.com/", 
         secure: false
       }
     }
